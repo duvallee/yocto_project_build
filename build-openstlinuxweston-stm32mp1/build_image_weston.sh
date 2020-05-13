3051d0ed393a810ea00f39ee7905c05799ac51db
duvallee.lee <duvalee@nate.com>
2020-05-13 15:15:55 +0900

    modified script file

--- build-openstlinuxweston-stm32mp1/build_image_weston.sh
+++ build-openstlinuxweston-stm32mp1/build_image_weston.sh
@@ -8,6 +8,7 @@ export END_TIME=""
 export TOP_DIR=`pwd`
 export DEPLOY_DIR=${TOP_DIR}/tmp-glibc/deploy/images/stm32mp1/
 export SDCARD_SHELL_SCRIPT=${DEPLOY_DIR}/scripts/create_sdcard_from_flashlayout.sh
+export FLASHLAYOUT_FILE=${DEPLOY_DIR}/flashlayout_st-image-weston/FlashLayout_sdcard_stm32mp157c-dk2-basic.tsv
 
 # ------------------------------------------------------------------
 # build_start_time
@@ -41,6 +42,7 @@ function build_end_time()
         mkdir -p ../release
     else
         rm -rf ../release
+        mkdir -p ../release
     fi
 
     if [ ! -e ${SDCARD_SHELL_SCRIPT} ]
@@ -48,5 +50,7 @@ function build_end_time()
         echo "Not found script : ${SDCARD_SHELL_SCRIPT} "
         exit 0
     fi
+    ${SDCARD_SHELL_SCRIPT} ${FLASHLAYOUT_FILE}
+    mv ${DEPLOY_DIR}/flashlayout_st-image-weston_FlashLayout_sdcard_stm32mp157c-dk2-basic.raw ../release/st32mp157c-dk2-basic.img 
 }
 
