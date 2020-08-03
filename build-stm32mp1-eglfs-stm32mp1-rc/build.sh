6fb7b0c10653819d3da06f7d83d396ed023af626
duvallee.lee <duvalee@nate.com>
2020-08-03 15:53:39 +0900

    add build script for image

---
+++ build-stm32mp1-eglfs-stm32mp1-rc/build.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+#
+#
+# ------------------------------------------------------------------
+export START_TIME=""
+export END_TIME=""
+
+export TOP_DIR=`pwd`
+export DEPLOY_DIR=${TOP_DIR}/tmp-glibc/deploy/images/stm32mp1-rc
+export SDCARD_SHELL_SCRIPT=${DEPLOY_DIR}/scripts/create_sdcard_from_flashlayout.sh
+export FLASHLAYOUT_FILE=${DEPLOY_DIR}/flashlayout_image-stm32mp1-rc/extensible/FlashLayout_sdcard_stm32mp157c-dk2-extensible.tsv
+
+# ------------------------------------------------------------------
+# build_start_time
+# ------------------------------------------------------------------
+function build_start_time()
+{
+   START_TIME=`date +%s`
+}
+
+# ------------------------------------------------------------------
+# build_end_time
+# ------------------------------------------------------------------
+function build_end_time()
+{
+   END_TIME=`date +%s`
+
+   echo "================================================================================="
+   echo "Build time : $((($END_TIME-$START_TIME)/60)) minutes $((($END_TIME-$START_TIME)%60)) seconds"
+   echo "================================================================================="
+}
+
+# ------------------------------------------------------------------
+# MAIN LOOP
+{
+   if [ ! -e ../../downloads ]
+   then
+      mkdir -p ../../downloads
+      ln -s ../../downloads downloads
+   fi
+
+   if [ ! -e downloads ]
+   then
+      ln -s ../../downloads downloads
+   fi
+
+   build_start_time
+
+   bitbake image-stm32mp1-rc -c cleanall
+   bitbake image-stm32mp1-rc
+
+   build_end_time
+
+   if [ ! -e ../release ]
+   then
+      mkdir -p ../release
+   else
+      rm -f ../release/st32mp157c-dk2-rc-car.img
+   fi
+
+   if [ ! -e ${SDCARD_SHELL_SCRIPT} ]
+   then
+      echo "Not found script : ${SDCARD_SHELL_SCRIPT} "
+      exit 0
+   fi
+
+   if [ -e ${DEPLOY_DIR}/FlashLayout_sdcard_stm32mp157c-dk2-extensible.raw ]
+   then
+      rm -f ${DEPLOY_DIR}/FlashLayout_sdcard_stm32mp157c-dk2-extensible.raw
+   fi 
+
+   ${SDCARD_SHELL_SCRIPT} ${FLASHLAYOUT_FILE}
+
+   mv ${DEPLOY_DIR}/FlashLayout_sdcard_stm32mp157c-dk2-extensible.raw ../release/st32mp157c-dk2-rc-car.img
+}
+
