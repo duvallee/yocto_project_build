676f7e8bd7073258d10489cb83fe7f2d142ef223
duvallee <duvallee@nate.com>
2021-04-08 09:19:48 +0900

    temp commit for qt

---
+++ build-qt-raspberrypi-4-64/build_qt_image.sh
@@ -0,0 +1,57 @@
+
+
+#!/bin/bash
+#
+#
+# ------------------------------------------------------------------
+export START_TIME=""
+export END_TIME=""
+
+# ------------------------------------------------------------------
+# build_start_time
+# ------------------------------------------------------------------
+function build_start_time()
+{
+   START_TIME=`date +%s`
+}
+
+
+# ------------------------------------------------------------------
+# build_end_time
+# ------------------------------------------------------------------
+function build_end_time()
+{
+   END_TIME=`date +%s`
+
+   echo ""
+   echo "================================================================================="
+   echo "${0} Build time : $((($END_TIME-$START_TIME)/60)) minutes $((($END_TIME-$START_TIME)%60)) seconds"
+   echo "================================================================================="
+   echo ""
+}
+
+
+# ------------------------------------------------------------------
+# MAIN LOOP
+{
+    if [ ! -e ../../downloads ]
+    then
+       mkdir -p ../../downloads
+       ln -s ../../downloads downloads
+    fi
+
+    if [ ! -e downloads ]
+    then
+       ln -s ../../downloads downloads
+    fi
+
+    build_start_time
+
+    bitbake image-raspberry-pi-qt -c cleanall
+    bitbake image-raspberry-pi-qt
+
+    build_end_time
+}
+
+
+
