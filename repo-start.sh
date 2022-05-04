6d42afd60ff79e2815e11fbea7e336b04b19a8d9
duvallee <duvallee@nate.com>
2022-05-04 18:07:08 +0900

    modified scripit file

--- repo-start.sh
+++ repo-start.sh
@@ -3,5 +3,73 @@
 #
 # ------------------------------------------------------------------
 
+pushd .
+cd dunfell
+repo start spotmicro-raspberry-pi 
+popd
 
+pushd .
+cd dunfell/bsp/u-boot 
+repo start raspberrypi_4  
+popd
+
+pushd .
+cd dunfell/bsp/kernel
+repo start raspberrypi_4  
+popd
+
+pushd .
+cd dunfell/source/poky 
+repo start dunfell  
+popd
+
+pushd .
+cd dunfell/source/meta-openembedded  
+repo start dunfell  
+popd
+
+pushd .
+cd dunfell/source/meta-qt5  
+repo start dunfell  
+popd
+
+pushd .
+cd dunfell/source/meta-raspberrypi  
+repo start dunfell  
+popd
+
+pushd .
+cd dunfell/source/meta-ros  
+repo start dunfell  
+popd
+
+pushd .
+cd dunfell/source/meta-raspberrypi-custom  
+repo start raspberrypi_4
+popd
+
+pushd .
+cd 3d-cad-files  
+repo start spotmicro-raspberry-pi 
+popd
+
+pushd .
+cd old-mcu  
+repo start spotmicro-raspberry-pi 
+popd
+
+pushd .
+cd micro-ros-mcu 
+repo start spotmicro-raspberry-pi-micro-ros 
+popd
+
+pushd .
+cd app/spotmicro  
+repo start spotmicro-raspberry-pi 
+popd
+
+pushd .
+cd app/test  
+repo start spotmicro-raspberry-pi-test  
+popd
 
